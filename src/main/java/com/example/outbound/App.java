052931ce1ed73667f90e2efafcb58f9216b21b10
Scott Harris <scott.harris@vonage.com>
2023-12-11 17:16:08 -0800

    initial checkin

---
+++ src/main/java/com/example/outbound/App.java
@@ -0,0 +1,56 @@
+package com.example.outbound;
+
+import com.vonage.client.VonageClient;
+import com.vonage.client.voice.AdvancedMachineDetection;
+import com.vonage.client.voice.AdvancedMachineDetection.Mode;
+import com.vonage.client.voice.Call;
+import com.vonage.client.voice.CallEvent;
+import com.vonage.client.voice.Endpoint;
+import com.vonage.client.voice.MachineDetection;
+import com.vonage.client.voice.PhoneEndpoint;
+import com.vonage.client.voice.VoiceClient;
+import com.vonage.client.voice.ncco.StreamAction;
+import com.vonage.client.voice.ncco.TalkAction;
+
+public class App {
+
+	// Replace these with your Vonage API credentials
+	private static final String APP_ID = "XXXXXXXXXXXXXXXXXXXXXXXXX";
+	private static final String PRIVATE_KEY = "outbound-calling.key";
+
+	public static void main(String[] args) {
+
+		final Endpoint TO_NUMBER = new PhoneEndpoint("14083142771");
+		final String VONAGE_NUMBER = "14084091740";
+
+		final String MEDIA_URL = "https://test-audio-streams.s3.amazonaws.com/countTo20.mp3";
+
+		VonageClient client = VonageClient.builder().applicationId(APP_ID).privateKeyPath(PRIVATE_KEY).build();
+
+		VoiceClient voiceClient = client.getVoiceClient();
+
+		/*
+		 * Uncomment this line to use TTS, instead of streaming an mp3 file. Make sure
+		 * you comment the StreamAction line, if you use TTS
+		 */
+		TalkAction action = TalkAction.builder(
+				"This is a text-to-speech call from Vonage. You will hear a lot more stuff, if you keep listening.")
+				.build();
+
+		/*
+		 * Uncomment this line to stream an mp3 file. Make sure you comment the /*
+		 * TalkAction line, above, if you stream uncomment the StreamAction line
+		 */
+		// StreamAction action = StreamAction.builder(MEDIA_URL).build();
+
+		AdvancedMachineDetection amd = AdvancedMachineDetection.builder().behavior(MachineDetection.CONTINUE)
+				.mode(Mode.DETECT_BEEP).beepTimeout(45).build();
+
+		Call call = Call.builder().to(TO_NUMBER).from(VONAGE_NUMBER).ncco(action).advancedMachineDetection(amd).build();
+
+		System.out.println("Making call with AMD");
+		CallEvent e = voiceClient.createCall(call);
+		System.out.println(e.toJson());
+
+	}
+}
